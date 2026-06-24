78b9b8271906d84a94548cad7add1abbf339b3a8
mizunatoma <k4017135@gmail.com>
2026-06-24 17:59:52 +0900

    chore: lintチェックをCIへ移行し、ビルド時のESLintを無効化 (#12)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // next build時のESLintチェックをスキップ
+    // lintの責務を「build」から「CI/CD」に移す
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
