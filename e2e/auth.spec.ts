ab5ba57d28e4c2bdd6b3644c380c5c045eb5a2b9
mizunatoma <k4017135@gmail.com>
2026-07-01 18:21:03 +0900

    test(e2e): 未ログイン時のルートガード(リダイレクト)を追加

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -8,3 +8,12 @@ test('ゲストログインでタイムラインに到達する', async ({ page 
   // 3. /user/timeline に遷移したか検証
   await expect(page).toHaveURL('/user/timeline')
 })
+
+test('未ログインで保護ページにアクセスしようとすると、/signinにリダイレクトされる', async ({
+  page,
+}) => {
+  // 1. 未ログイン状態で保護ページを開く
+  await page.goto('/user/timeline')
+  // 2. middlewareで /signin に飛ばされることを検証
+  await expect(page).toHaveURL('/signin')
+})
