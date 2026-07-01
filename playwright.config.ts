afce566385ddb2845a5cdddc675db751159cda4c
mizunatoma <k4017135@gmail.com>
2026-07-01 17:11:35 +0900

    chore(test): Playwright(E2E)環境を構築

---
+++ playwright.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './e2e',
+  use: {
+    baseURL: 'http://localhost:3000',
+  },
+  webServer: {
+    command: 'npm run build && npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
+    timeout: 120 * 1000,
+  },
+})
